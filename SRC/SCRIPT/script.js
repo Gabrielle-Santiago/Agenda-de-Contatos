64120c21512e0c4f3e6d6af671289b6d695f419f
Gabrielle Santiago Silva <gabriellesantisilva@gmail.com>
2024-02-13 21:22:56 -0300

    Conexão com js com o banco e redução do código

---
+++ SRC/SCRIPT/script.js
@@ -0,0 +1,10 @@
+/*if($nome == "" || $nome == null){
+    alert("O campo nome deve ser preenchido!!");
+    window.location.href="cadast.php"
+}*/
+
+//informar ao usuário se a conexão com o banco foi bem sucedida
+alert("Teste 0")
+function verificar(params) {
+  alert("teste")
+}
