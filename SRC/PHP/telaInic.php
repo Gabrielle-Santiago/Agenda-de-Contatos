f8ef05940aa0e7f1d23c92b291f0dece0f2b1a12
Gabrielle Santiago Silva <gabriellesantisilva@gmail.com>
2024-02-22 14:29:41 -0300

    Finalização da tela principal

--- SRC/PHP/telaInic.php
+++ SRC/PHP/telaInic.php
@@ -6,31 +6,32 @@
     <title>Tela Inicial</title>
 </head>
 <body>
-    <?php
-    //esta dando erro com o con (diz que não identifica)
+<?php
+    include_once('banco.php');
 
-        $sql = "SELECT * FROM contatos";
+    $sql = "SELECT * FROM contatos";
 
-        $result = $con -> query ($sql);
+    $resultado = $con->query($sql);
 
-        $qtd = $result -> num_rows;
+    if ($resultado->num_rows > 0) {
 
-        if ($qtd > 0) {
-            print"<table class= 'table table hover table-striped'>";
-            while ($row = $result -> fetch_object()//recuperar uma linha do resultado da consulta como um objeto.
-            ) {
-                print "<tr>";
-                print "<td>". $row -> id;
-                print "<td>". $row -> nome;
-                print "<td>". $row -> senha;
-                print "<td>". $row -> genero;
-                print "<td>". $row -> datNasc;
-                print "</tr>";
-            }
-            print "</table>";
-        } else{
-            print "<p class='alert alert-danger'>Não foram encontrados resultados!</p>";
+        echo "<table class='table table-hover table-striped'>";
+        echo "<tr><th>ID</th><th>Nome</th><th>Senha</th><th>Gênero</th><th>Data de Nascimento</th></tr>";
+        while ($row = $resultado->fetch_object())
+        {
+            echo "<tr>";
+            echo "<td>" . $row -> id . "</td>";
+            echo "<td>" . $row -> nome . "</td>";
+            echo "<td>" . $row -> senha . "</td>";
+            echo "<td>" . $row -> genero . "</td>";
+            echo "<td>" . $row -> datNasc . "</td>";
+            echo "</tr>";
         }
-    ?>
+        echo "</table>";
+        } else {
+            echo "<p class='alert alert-danger'>Não foram encontrados resultados!</p>";
+        }
+?>
+
 </body>
 </html>
